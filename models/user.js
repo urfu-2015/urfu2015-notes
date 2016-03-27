06eb327b7148a4fd793e3dd719a727d9dabeef81
Evgeny Mokeev <mokeev@yandex-team.ru>
2016-03-27 12:48:47 +0500

    Безопасное хранение паролей

--- models/user.js
+++ models/user.js
@@ -1,17 +1,40 @@
 'use strict';
 
+const crypto = require('crypto');
+
 const users = [
-    {id: 1, username: 'admin', password: 'admin'},
-    {id: 2, username: 'user', password: 'user'}
+    {id: 1, username: 'admin', hash: '0192023a7bbd73250516f069df18b500', salt: '123'},
+    {id: 2, username: 'user', hash: 'f67c683f0f3e98cb9dd5582e8cbbcd04', salt: '321'}
 ];
 
+function encryptPassword(password, salt) {
+    return crypto.createHash('md5').update(password + salt).digest('hex');
+}
+
 class User {
-    static find(username, password) {
-        return users.find(user => user.username === username && user.password === password);
+    constructor(props) {
+        this.id = props.id;
+        this.username = props.username;
+
+        this.hash = props.hash;
+        this.salt = props.salt;
+    }
+    checkPassword(password) {
+        const hash = encryptPassword(password, this.salt);
+
+        return this.hash === hash;
+    }
+
+    static findByName(username) {
+        var user = users.find(user => user.username === username);
+
+        return user && new User(user);
     }
 
     static findById(id) {
-        return users.find(user => user.id === id);
+        var user = users.find(user => user.id === id);
+
+        return user && new User(user);
     }
 
     static getSerializator() {
